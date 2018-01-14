58f9be00ea57722735800ed37496abd12f335a58
adsznzhang <adszn@hotmail.com>
2018-01-14 22:20:19 +1200

    Add mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
 const authRoutes = require('./routes/authRoutes.js');
 require('./services/passport.js');
 
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
