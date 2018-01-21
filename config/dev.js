8ec98a012164b35c9b6e1d62274b9febbf1da99b
adsznzhang <adszn@hotmail.com>
2018-01-21 20:41:15 +1200

    debug type error

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev.js dont commit this
+
+module.exports = {
+    googleClientID: '245954508272-4pe5vcak2v121a6nhk1m3svkecvoj068.apps.googleusercontent.com',
+    googleClientSecret: 'XyX8hQLfBPoKFPh1Iw3myNCK',
+    mongoURI: 'mongodb://adsznzhang:Zhangzhen12@ds157097.mlab.com:57097/emailydev',
+    cookieKey: 'sdfoweifooooopasodifwiero'
+};
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {Schema} = mogoose;
+const {Schema} = mongoose;
 
 const userSchema = new Schema({
 
