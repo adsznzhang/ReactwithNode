611baab13fd64b1b1b16fadad6344be61dd088ee
adsznzhang <adszn@hotmail.com>
2018-02-03 20:04:52 +1200

    Add environments into React

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
     googleClientID: '245954508272-4pe5vcak2v121a6nhk1m3svkecvoj068.apps.googleusercontent.com',
     googleClientSecret: 'XyX8hQLfBPoKFPh1Iw3myNCK',
     mongoURI: 'mongodb://adsznzhang:Zhangzhen12@ds157097.mlab.com:57097/emailydev',
-    cookieKey: 'sdfoweifooooopasodifwiero'
+    cookieKey: 'sdfoweifooooopasodifwiero',
+    stripePublishableKey: 'pk_test_gpLWZwtrrsa9ddB66mt6s5ED',
+    stripeSecretKey: 'sk_test_rfQCQfcQxf9mezaexCVjlpAm'
 };
