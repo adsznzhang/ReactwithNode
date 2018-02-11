21149e0cf5523e5cbc8429ecf1d38651ce35c1a2
adsznzhang <adszn@hotmail.com>
2018-02-11 21:36:51 +1200

    try ...

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,19 @@
 //prod.js - production
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI:process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY,
-    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+    googleClientID: '245954508272-4pe5vcak2v121a6nhk1m3svkecvoj068.apps.googleusercontent.com',
+    googleClientSecret: 'XyX8hQLfBPoKFPh1Iw3myNCK',
+    mongoURI: 'mongodb://adsznzhang:Zhangzhen12@ds157097.mlab.com:57097/emailydev',
+    cookieKey: 'sdfoweifooooopasodifwiero',
+    stripePublishableKey: 'pk_test_gpLWZwtrrsa9ddB66mt6s5ED',
+    stripeSecretKey: 'sk_test_rfQCQfcQxf9mezaexCVjlpAm'
 };
+
+// module.exports = {
+//     googleClientID: process.env.GOOGLE_CLIENT_ID,
+//     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     mongoURI:process.env.MONGO_URI,
+//     cookieKey: process.env.COOKIE_KEY,
+//     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+//     stripeSecretKey: process.env.STRIPE_SECRET_KEY
+// };
 
